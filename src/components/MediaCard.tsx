9999e164dfaa43312f44e28262ee1cd2738ccb02
Ryan Rozema <rrozema2@gmail.com>
2020-11-09 15:36:11 -0800

    import image bug

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -13,7 +13,7 @@ import IconButton from "@material-ui/core/IconButton";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
 
-import ImageNotFound from "../img/image_not_found.png";
+import ImageNotFound from "../images/image_not_found.png";
 import { IWatchList } from "../types/streams";
 
 function truncate(str: string, n: number) {
